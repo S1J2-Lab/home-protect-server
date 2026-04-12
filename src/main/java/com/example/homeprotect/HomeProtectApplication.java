b5693fbef05a2b31436d758e5a63a3fb5667a889
jjhparkk <nomoredkdlel@gmail.com>
2026-04-12 13:00:04 +0900

    chore: 프로젝트 초기 설정 및 환경 구축

---
+++ src/main/java/com/example/homeprotect/HomeProtectApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeprotect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProtectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HomeProtectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/homeprotect/HomeProtectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeprotect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProtectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
