309a496321ac848909eb36eaa5aebdb8aec7bdc9
jjhparkk <nomoredkdlel@gmail.com>
2026-05-07 18:38:44 +0900

    refactor: 배포 주소 추가 (#41)

--- src/main/java/com/example/homeprotect/config/SecurityConfig.java
+++ src/main/java/com/example/homeprotect/config/SecurityConfig.java
@@ -19,7 +19,6 @@ public class SecurityConfig {
         "http://localhost:5173",
         "https://d376fu0rzbcwop.cloudfront.net",
         "https://homeprotect.shop"
-        // 프로덕션 도메인 추가 시 여기에 append
     );
 
     @Bean
